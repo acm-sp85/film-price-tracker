d709be821a1b32e809ceb574d46d41c2ff0aacaa
Alex-new-laptop <acontell@gmail.com>
2023-08-07 17:00:05 +0200

    front and back end talking

--- film-o/src/app/page.js
+++ film-o/src/app/page.js
@@ -1,6 +1,15 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './page.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3000/films')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+
   return <main className={styles.main}>Film-O</main>;
 }
